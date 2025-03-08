2f040af73014b2ab3135ab45ab0326c1e013df30
Mohamed Bashar <mohamednbashar@gmail.com>
2025-03-08 23:33:57 +0200

    add assignments solutions

---
+++ Assignments_5/task_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Test Case 1
+    int age = 18;
+    int points = 450;
+    
+    if (age > 18)
+        cout << "Yes Age Is Ok\n";
+    else 
+        cout << "No Age Is Not Ok\n";
+
+    if (points > 500)
+        cout << "Yes Points Is Ok\n";
+    else
+        cout << "No Points Is Not Ok\n";
+        
+    return 0;
+}
